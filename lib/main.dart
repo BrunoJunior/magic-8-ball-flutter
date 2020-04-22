05092ec39b4ee83c5e79eab47f26f9dd548f279d
Thalassa Ingénierie Web <thalassa.dev@gmail.com>
2020-04-22 17:00:12 +0200

    My Magic Ball with shake event

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,63 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shake_event/shake_event.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> with ShakeHandler {
+  int response;
+  _BallState() {
+    changeResponse();
+  }
+
+  /// Change the response
+  void changeResponse() => response = 1 + Random().nextInt(5);
+
+  @override
+  void dispose() {
+    resetShakeListeners();
+    super.dispose();
+  }
+
+  @override
+  shakeEventListener() {
+    setState(changeResponse);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    startListeningShake(30.0);
+    return Center(
+      child: FlatButton(
+        onPressed: shakeEventListener,
+        child: Image.asset(
+          'images/ball$response.png',
+        ),
+      ),
+    );
+  }
+}
